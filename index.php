e53cbba6f7cdcd629da09b3b6922151a99389d29
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-12-11 15:22:12 +0600

    registration

--- index.php
+++ index.php
@@ -89,29 +89,33 @@
 <!-- =============modal Login end===================== -->
 <!-- =============modal Register start===================== -->
 <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <form >
       <div class="modal-header">
         <h5 class="modal-title d-flex align-items-center">
-        <i class="bi bi-person-circle fs-3 me-2"></i>
-          User Login
+        <i class="bi bi-person-lines-fill fs-3 me-2"></i>
+          User Registration
         </h5>
         <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-      <div class="mb-3">
+      <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+        Note: Your details must with your ID (Aadhaar card, passport, driving license, etc.)
+        that will be required during check-in.
+      </span>
+    <!-- <div class="mb-3">
         <label  class="form-label">Email address</label>
         <input type="email" class="form-control shadow-none" >
-      </div>
-      <div class="mb-4">
+    </div>
+    <div class="mb-4">
         <label  class="form-label">Password</label>
         <input type="password" class="form-control shadow-none" >
-      </div>
-      <div class="d-flex align-items-center justify-content-between mb-2">
-      <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-     <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
     </div>
+    <div class="d-flex align-items-center justify-content-between mb-2">
+      <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+      <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+    </div> -->
   </div>
       </form>
     
