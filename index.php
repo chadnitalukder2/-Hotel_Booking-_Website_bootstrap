c4a1b05371f7ad2180639df76206229aaa9b7e9c
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-12-11 15:03:11 +0600

    login

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Hotel Booking Website</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 
 </head>
 <style>
@@ -17,53 +18,107 @@
     }
 </style>
 <body>
-    
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- =============nav start===================== -->
+<nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">NP HOTEL</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">NP HOTEL</a>
+    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Rooms</a>
+          <a class="nav-link me-2" href="#">Rooms</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Facilities</a>
-        </li>
-        <li class="nav-item">4
-          <a class="nav-link" href="#">Contact us</a>
+          <a class="nav-link me-2" href="#">Facilities</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Rooms</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <a class="nav-link me-2" href="#">Contact us</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link " href="#">About</a>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <div class="d-flex">
+        <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+          Login
+        </button>
+        <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+          Register
+        </button>
+  </div>
     </div>
   </div>
 </nav>
-
+ <!-- =============nav end===================== -->
+<!-- =============modal login start===================== -->
+<div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form >
+      <div class="modal-header">
+        <h5 class="modal-title d-flex align-items-center">
+        <i class="bi bi-person-circle fs-3 me-2"></i>
+          User Login
+        </h5>
+        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <div class="mb-3">
+        <label  class="form-label">Email address</label>
+        <input type="email" class="form-control shadow-none" >
+      </div>
+      <div class="mb-4">
+        <label  class="form-label">Password</label>
+        <input type="password" class="form-control shadow-none" >
+      </div>
+      <div class="d-flex align-items-center justify-content-between mb-2">
+      <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+     <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+    </div>
+  </div>
+      </form>
+    
+    </div>
+  </div>
+</div>
+<!-- =============modal Login end===================== -->
+<!-- =============modal Register start===================== -->
+<div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form >
+      <div class="modal-header">
+        <h5 class="modal-title d-flex align-items-center">
+        <i class="bi bi-person-circle fs-3 me-2"></i>
+          User Login
+        </h5>
+        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <div class="mb-3">
+        <label  class="form-label">Email address</label>
+        <input type="email" class="form-control shadow-none" >
+      </div>
+      <div class="mb-4">
+        <label  class="form-label">Password</label>
+        <input type="password" class="form-control shadow-none" >
+      </div>
+      <div class="d-flex align-items-center justify-content-between mb-2">
+      <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+     <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+    </div>
+  </div>
+      </form>
+    
+    </div>
+  </div>
+</div>
+<!-- =============modal Register end===================== -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
