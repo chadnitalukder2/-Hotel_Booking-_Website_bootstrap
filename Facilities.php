d5150b55d3cf90f48a44e67fec8f8b1e91923825
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-12-13 00:51:48 +0600

    facilities

---
+++ Facilities.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NP Hotel - Home</title>
+    <?php require("inc./links.php"); ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+</head>
+
+<body class="bg-light">
+
+ <!-- =============header start ===================== -->
+<?php require("inc/header.php"); ?>
+<!-- =============Facilities start ===================== -->
+<div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+</div>
+<!-- =============Facilities end ===================== -->
+<!-- =============Footer start===================== -->
+<?php require("inc./footer.php"); ?>
+
+</body>
+</html>
