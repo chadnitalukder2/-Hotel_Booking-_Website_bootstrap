b5d9f9899211a34a819f3303f9c4cafc9e9d1640
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-12-13 14:23:00 +0600

    about

--- Facilities.php
+++ Facilities.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NP Hotel - Home</title>
+    <title>NP Hotel - Facilities</title>
     <?php require("inc./links.php"); ?>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+   
 <style>
     .pop:hover{
         border-top-color: var(--teal) !important;
