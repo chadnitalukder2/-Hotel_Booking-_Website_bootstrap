9c5752d520ebae5e7acb1817a4585ad2fa31a7ba
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-12-13 10:10:37 +0600

    facilities

--- Facilities.php
+++ Facilities.php
@@ -6,6 +6,13 @@
     <title>NP Hotel - Home</title>
     <?php require("inc./links.php"); ?>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+<style>
+    .pop:hover{
+        border-top-color: var(--teal) !important;
+        transform: scale(1.03);
+        transition: all .3s;
+    }
+</style>
 </head>
 
 <body class="bg-light">
@@ -15,6 +22,81 @@
 <!-- =============Facilities start ===================== -->
 <div class="my-5 px-4">
     <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+        Et eos eirmod consetetur est justo consetetur invidunt.Nonumy diam sed ut <br>
+          sea lorem, aliquyam tempor est accusam ipsum sadipscing.
+    </p>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px" alt="">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>Lorem takimata dolores sit tempor lorem.
+                   Sit sea rebum clita sit et accusam, sit justo erat.
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px" alt="">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>Lorem takimata dolores sit tempor lorem.
+                   Sit sea rebum clita sit et accusam, sit justo erat.
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px" alt="">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>Lorem takimata dolores sit tempor lorem.
+                   Sit sea rebum clita sit et accusam, sit justo erat.
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px" alt="">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>Lorem takimata dolores sit tempor lorem.
+                   Sit sea rebum clita sit et accusam, sit justo erat.
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px" alt="">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>Lorem takimata dolores sit tempor lorem.
+                   Sit sea rebum clita sit et accusam, sit justo erat.
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                    <img src="images/facilities/wifi.svg" width="40px" alt="">
+                    <h5 class="m-0 ms-3">Wifi</h5>
+                </div>
+                <p>Lorem takimata dolores sit tempor lorem.
+                   Sit sea rebum clita sit et accusam, sit justo erat.
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- =============Facilities end ===================== -->
 <!-- =============Footer start===================== -->
